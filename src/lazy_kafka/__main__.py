0029515f713c599207a90f431f442b0f39d84d6d
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-06-27 15:43:44 +0200

    feat: prototype with databinding

--- src/lazy_kafka/__main__.py
+++ src/lazy_kafka/__main__.py
@@ -8,16 +8,17 @@ from textual.containers import ScrollableContainer
 from textual.widget import Widget
 from textual.widgets import Button, Footer, Header, Static, ListItem, ListView, Label
 from rich.panel import Panel
-from textual.widgets import Tree, Pretty
+from textual.widgets import Tree, Pretty, Rule
 from textual.widgets.tree import TreeNode
 from textual.color import Color
 from textual.message import Message
+from dataclasses import dataclass
 
 import logging
-
+logging.basicConfig(level=logging.INFO)
+from textual import log
 CONSOLE = rich.console.Console()
 print(textual.__version__)
-
 
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer
@@ -26,112 +27,81 @@ from textual.widgets import Button, Footer, Header, Static
 
 from .kafka import *
 
-class TopicDetails(Widget):
+@dataclass
+class TopicData:
+    topic: str
+    partitions: object
 
-    topic_details = reactive({"no":"det"})
-
-    def compose(self):
-        yield Pretty(self.topic_details)
-
-    def watch_topic_details(self, topic_details):
-        try:
-            self.query_one(Pretty).update(topic_details)
-        except:
-            pass
-
-class TopicList(Widget):
+class Topic(Static):
     """A widget to display elapsed time."""
-    BINDINGS = [
-        ("a", "add_stopwatch", "Add"),
-        ("h", "vleft", "Move left"),
-        ("j", "vdown", "Motion down"),
-        ("k", "vup", "up"),
-        ("l", "vright", "right"),
-        ("x", "details", "get details"),
-    ]
     
     class Selected(Message):
-        
-        def __init__(self, topic: str) -> None:
+        """Color selected message."""
+
+        def __init__(self, topic: TopicData) -> None:
+            log(f"INIT: {topic!r}")
             self.topic = topic
             super().__init__()
-
-    def action_details(self) -> None:
-        """Show details in TopicDetails widget."""
-        self.query_one(TopicDetails).details = {"hello": {"world": "!"}}
-
-    def action_toggle_dark(self) -> None:
-        """An action to toggle dark mode."""
-        self.dark = not self.dark
     
-    def action_vright(self) -> None:
-        """Add a node to the tree."""
-        tree = self.query_one(Tree)
-        tree.root.expand()
 
-    def action_vleft(self) -> None:
-        """Add a node to the tree."""
-        tree = self.query_one(Tree)
-        tree.root.collapse()
+    def __init__(self, topic: TopicData) -> None:
+        self.topic = topic
+        super().__init__()
 
-    def action_vdown(self) -> None:
-        """Add a node to the tree."""
-        tree = self.query_one(Tree)
-        tree.action_cursor_down()
-
-    def action_vup(self) -> None:
-        """Add a node to the tree."""
-        tree = self.query_one(Tree)
-        tree.action_cursor_up()
-        tree.show_guides=False
-
-    def on_click(self):
+    def on_click(self) -> None:
+        # The post_message method sends an event to be handled in the DOM
         self.post_message(self.Selected(self.topic))
 
-    def compose(self):
-        tree = Tree("root", id="topicTree")
-        tree.root.expand()
-        j = list_topics()
-        for t in j:
-            tree.root.add_leaf(t.topic)
-        yield tree
+    def on_mount(self) -> None:
+        self.styles.margin = (1, 2)
+        self.styles.content_align = ("center", "middle")
+        self.styles.background = Color.parse("#ffffff33")
+
+    def render(self) -> str:
+        return str(self.topic.topic)
 
 class TopicPanel(Static):
     """A stopwatch widget."""
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        """Event handler called when a button is pressed."""
-        if event.button.id == "start":
-            self.add_class("started")
-        elif event.button.id == "stop":
-            self.remove_class("started")
+    def compose(self):
+        j = list_topics()
+        self.log("here")
+        for t in j:
+            yield Topic(t)
 
-    def compose(self) -> ComposeResult:
-        """Create child widgets of a stopwatch."""
-        yield ScrollableContainer(TopicList())
+
+class TopicDetails(Widget):
+    topic = reactive("")
+
+    def render(self) -> str:
+        return f"[b]TOPIC:[/b] {self.topic}"
 
 class TopicPanelApp(App):
     """A Textual app to manage stopwatches."""
 
-    CSS_PATH = "style.tcss"
-    topic_details = reactive({})
-
-    def on_topic_selected(self, message: TopicList.Selected):
-        logging.info(message)
+    #CSS_PATH = "style.tcss"
 
     def on_mount(self):
         label = self.query_one("#topic")
         label.border_title = "Topics"
         label.border_subtitle = "status"
-        self.panel.styles.color = Color(191, 78, 96)
+        #self.panel.styles.color = Color(191, 78, 96)
+        self.log(self.tree)
 
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
-        self.panel = ScrollableContainer(TopicPanel(), id="topic", classes="box")
         yield Header()
-        yield self.panel
-        yield TopicDetails(classes="box").data_bind(TopicPanelApp.topic_details)
+        yield TopicPanel(id="topic")
+        #yield TopicDetails(classes="box")
+        yield Rule(line_style="thick")
+        yield Rule(line_style="dashed")
+        yield TopicDetails()
         yield Footer()
+
+    def on_topic_selected(self, message: Topic.Selected) -> None:
+        self.log(message.topic)
+        self.query_one(TopicDetails).topic = "topic: "+str(message.topic.topic)+str(message.topic.partitions)
+        self.log(f"------------------{message.topic}")
 
 app = TopicPanelApp()
 
