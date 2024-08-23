771d126be7c3841d4f88b71c9623df8fbdb4cb53
Timon Viola <44016238+timonviola@users.noreply.github.com>
2024-08-23 21:05:40 +0200

    feat: add schema registry docker

---
+++ scripts/create_schema.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Create schema, associate with topic
+#
+# Generic request form [reference](https://docs.confluent.io/platform/current/schema-registry/develop/using.html)
+#   curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+#     --data '{"schema": "{\"type\": \"string\"}"}' \
+#     http://localhost:8081/subjects/Kafka-value/versions
+
+set -x
+
+until curl --output /dev/null --silent $SCHEMA_REGISTRY_URL/; do
+      >&2 echo "Server unavailable - sleeping"
+      sleep 10
+done
+
+response=$(curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" \
+    --write-out '%{http_code}' --output /dev/null \
+    --data '{"schema": "{\"type\": \"string\"}"}' \
+    $SCHEMA_REGISTRY_URL/subjects/$SUBJECT/versions
+)
+
+if [ "$response" -eq 200 ]; then
+    >&2 echo "Schema created successfully."
+    exit 0
+else
+    >&2 echo "Schema creation failed."
+    exit 1
+fi
