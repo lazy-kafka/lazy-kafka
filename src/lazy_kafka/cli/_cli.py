d4f87c940b42bbb7e44ab060ff3080527fad1b47
Timon Viola <44016238+timonviola@users.noreply.github.com>
2025-06-18 18:25:57 +0200

    feat: clean up cli

--- src/lazy_kafka/cli/_cli.py
+++ src/lazy_kafka/cli/_cli.py
@@ -1,27 +1,73 @@
 """Main CLI app to manage common config."""
+from pathlib import Path
 import typer
 from typing_extensions import Annotated
+from typing import Union
+import logging
 
 from . import _kafka
-from . import _version
+from lazy_kafka import __version__
+from lazy_kafka.config import Configuration
+from rich import print
+from rich.logging import RichHandler
 
 __all__ = ["app"]
 
-app = typer.Typer(rich_markup_mode="rich", callback=_version.callback)
+def _set_up_logging(level = "INFO"):
+    FORMAT = "%(message)s"
+    print(level)
+    logging.basicConfig(
+        level=level, format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True)]
+    )
+
+
+app = typer.Typer(rich_markup_mode="rich")
 
 app.add_typer(_kafka.app, name="kafka")
 
+def version_callback(value: bool) -> None:
+    if value:
+        print(f"LazyKafka CLI version: [green]{__version__}[/green]")
+        raise typer.Exit()
+
 @app.callback()
-def config_file(
+def global_options(
     config_file: Annotated[
-        bool,
+        Path,
         typer.Option(
             "--config-file",
+            "-f",
             help="Path to lazy-kafka config file.",
             rich_help_panel="[i][green]Customization and Utils[/]"
         ),
-] = False,
+    ] = Configuration.default_config_file_path(),
+    verbose: Annotated[
+        bool,
+        typer.Option(
+            "--verbose",
+            "-v",
+            help="Logging verbosity.",
+            rich_help_panel="Options"
+        ),
+    ] = False,
+    version: Annotated[
+        Union[bool, None],
+        typer.Option(
+            "--version",
+            help="Show the version and exit.",
+            callback=version_callback,
+            is_eager=True
+        ),
+    ] = None,
 ):
     """Overwrite the default app config."""
-    print(f"hello {config_file}")
+    # Configure app.
+    if verbose:
+        _set_up_logging(level="DEBUG")
+    else:
+        _set_up_logging()
+
+    _LOGGER = logging.getLogger(__name__)
+    _LOGGER.debug(f"LazyKafka v{__version__} {config_file=} {verbose=}")
+
 
--- src/lazy_kafka/cli/_kafka.py
+++ src/lazy_kafka/cli/_kafka.py
@@ -1,25 +1,41 @@
 """Apache Kafka related CLI."""
+
 from __future__ import annotations
 import logging
+import json
+from pathlib import Path
 
 import rich.progress
 import typer
-from confluent_kafka import Consumer
-from confluent_kafka.serialization import MessageField, SerializationContext
+import random
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.serialization import (
+    MessageField,
+    SerializationContext,
+    StringSerializer,
+)
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from typing_extensions import Annotated
 from typing import Union
 from rich import print
 
+from time import sleep
+from uuid import uuid4
 from lazy_kafka.config import Configuration
 
 
+CONSOLE = rich.console.Console(log_path=False)
+
 app = typer.Typer()
+
 
 @app.callback()
 def kafka():
     """Interact with [b]Apache Kafka[/]."""
 
-def consume_generator(consumer, topic: str,  max_messages = None) :
+
+def consume_generator(consumer, topic: str, max_messages=None):
     consumer.subscribe([topic])
     message_count = 0
 
@@ -34,20 +50,39 @@ def consume_generator(consumer, topic: str,  max_messages = None) :
 
                 message_count += 1
                 yield (
-                    msg.value(), SerializationContext(msg.topic(), MessageField.VALUE)
+                    msg.value(),
+                    SerializationContext(msg.topic(), MessageField.VALUE),
                 )
             except KeyboardInterrupt:
                 break
     finally:
         consumer.close()
 
-@app.command()
-def consume(topic: Annotated[str, typer.Argument(help="Name of the topic", show_default=False)]):
+
+# NOTE: the commands below repeat the config file path option, this is due
+# to ctx object not playing nice and me not grasping fully:
+# https://github.com/fastapi/typer/discussions/1195
+@app.command(
+    context_settings={"allow_extra_args": True, "ignore_unknown_options": True}
+)
+def consume(
+    ctx: typer.Context,
+    topic: Annotated[str, typer.Argument(help="Name of the topic", show_default=False)],
+    config_file: Annotated[
+        Path,
+        typer.Option(
+            "--config-file",
+            "-f",
+            help="Path to lazy-kafka config file.",
+            rich_help_panel="[i][green]Customization and Utils[/]"
+        ),
+    ] = Configuration.default_config_file_path(),
+):
     """Watch for new messages in [i]`topic`[/] until stopped.
 
     [blue]🐣 lazy-kafka ➜ python src/lazy_kafka/scripts/topic_schema_producer.py
     """
-    cfg = Configuration().from_local_config()
+    cfg = Configuration().from_toml(config_file)
     consumer = Consumer(cfg.kafka.to_config())
 
     consumer_iterable = consume_generator(consumer, topic)
@@ -68,10 +103,104 @@ def consume(topic: Annotated[str, typer.Argument(help="Name of the topic", show_
                 # continue polling even if no more new msg
                 continue
 
+
 @app.command()
-def produce(topic: Annotated[str, typer.Argument(help="Name of the topic", show_default=False)]):
-    # TODO: kafka subpackage and consue/produce files? - probably too granular
-    print("TODO: scripts/topic_schema_producer")
-    
+def produce(
+    topic: Annotated[str, typer.Argument(help="Name of the topic", show_default=False)],
+    config_file: Annotated[
+        Path,
+        typer.Option(
+            "--config-file",
+            "-f",
+            help="Path to lazy-kafka config file.",
+            rich_help_panel="[i][green]Customization and Utils[/]"
+        ),
+    ] = Configuration.default_config_file_path(),
+):
+    """Produce new messages in [i]`topic`[/] until stopped.
+
+    The producer functionality is only for testing purposes.
+
+    Produce messages in different ways:
+      - no schema, raw payload
+      - JSONSchema
+      - AVRO Schema
+      - Protobuff
+    """
+    print("Press <C>+C to stop producing messages.")
+    _produce(topic, config_file)
 
 
+def _produce(topic: str, config_file:Path):
+
+
+    def _data_to_dict(_d: dict, ctx: Any) -> dict:  # noqa: ARG001, ANN401
+        return _d
+
+    cfg = Configuration().from_toml(config_file)
+    producer = Producer(cfg.kafka.to_config())
+    string_serializer = StringSerializer("utf_8")
+
+    print(f"Producing user records to topic {topic}. ^C to exit.")
+
+    def _user():
+        return json.dumps(
+            {
+                "confidence": random.random(),
+                "createdDateTimeUTC": "2025-06-18T13:08:20.423076",
+                "isRain": True,
+                "validUntil": "2025-06-18T13:09:20.423076",
+                "snow": {"shape": "good"},
+            }
+        )
+
+        return dict(
+            name=str([chr(random.randint(97, 122)) for _ in range(4)]),
+            favorite_number=random.randint(0, 10),
+            favorite_color=random.choice(["blue", "red"]),
+        )
+
+    with rich.progress.Progress(
+        rich.progress.SpinnerColumn(),
+        rich.progress.TextColumn("[progress.description]{task.description}"),
+        rich.progress.BarColumn(style="blue", pulse_style="#3879ba"),
+        rich.progress.TimeElapsedColumn(),
+        transient=False,
+        console=CONSOLE,
+    ) as prog:
+        prog.add_task("[bold]Producing", total=None)
+
+        def _delivery_report(err, msg):
+            """
+            Reports the success or failure of a message delivery.
+
+            Args:
+                err (KafkaError): The error that occurred on None on success.
+                msg (Message): The message that was produced or failed.
+            """
+            if err is not None:
+                _LOGGER.info(f"Delivery failed for User record {msg.key()}: {err}")
+                return
+            prog.log(
+                f"Record {msg.key()} successfully produced to [blue]{msg.topic()}[/] partition-{msg.partition()} at offset [orange]{msg.offset()}[/]."
+            )
+
+        while True:
+            # Serve on_delivery callbacks from previous calls to produce()
+            producer.poll(0.0)
+            try:
+                producer.produce(
+                    topic=topic,
+                    key=string_serializer(str(uuid4())),
+                    value=_user(),
+                    on_delivery=_delivery_report,
+                )
+                sleep(1)
+            except KeyboardInterrupt:
+                break
+            except ValueError:
+                print("Invalid input, discarding record...")
+                continue
+
+    print("\n🌪️🚽Flushing final records...")
+    producer.flush()
